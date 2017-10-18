a362a08eb5f5d03c90f3943ab47974cd81e342d7
Hacker <MidhunHaridas>
2017-10-18 13:13:06 +0530

    add get todos id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -30,6 +31,23 @@ app.get('/todos', (req, res) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
+	});
+});
+
+// get by todo by id
+app.get('/todos/:id', (req, res) => {
+	var todoId = req.params.id;
+	if(!ObjectID.isValid(todoId)) {
+		return res.status(404).send({msg:"Id not valid"});
+	}
+
+	Todo.findById(todoId).then((todo) => {
+	if(!todo) {
+		return res.status(400).send({msg:"todo not available."});
+	}
+	res.send({todo});
+}).catch((e) => {
+	return res.status(401).send({msg:"error"});
 	});
 });
 
