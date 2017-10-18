a065ce99bc57c1baa40872106e8f004633e5ce8b
Hacker <MidhunHaridas>
2017-10-18 12:09:01 +0530

    Add post todo routes and refactor mongooose

---
+++ server/db/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// assigning global Promise to mongoose Promise object
+mongoose.Promise = global.Promise;
+// connecting to mongo db
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+// use the below if deprecation warning pops up
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+  useMongoClient: true
+});
+
+module.exports = {
+	mongoose: mongoose
+};
---
+++ server/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = new express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// create todo
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+ 		res.status(400).send(e);
+	});
+});
+
+
+// start server
+app.listen(port, () => {
+	console.log(`Server on port ${port}`);
+});
